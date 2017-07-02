b3d9a3906de1d332f24c8563b4e2c1e6c84fb463
Darong Mean <darong012@gmail.com>
2017-07-02 12:50:33 +0700

    init calculator

---
+++ calculator/src/clj/calculator/application.clj
@@ -0,0 +1,25 @@
+(ns calculator.application
+  (:gen-class)
+  (:require [com.stuartsierra.component :as component]
+            [system.components.endpoint :refer [new-endpoint]]
+            [system.components.handler :refer [new-handler]]
+            [system.components.middleware :refer [new-middleware]]
+            [system.components.jetty :refer [new-web-server]]
+            [calculator.config :refer [config]]
+            [calculator.routes :refer [home-routes]]))
+
+(defn app-system [config]
+  (component/system-map
+   :routes     (new-endpoint home-routes)
+   :middleware (new-middleware {:middleware (:middleware config)})
+   :handler    (-> (new-handler)
+                   (component/using [:routes :middleware]))
+   :http       (-> (new-web-server (:http-port config))
+                   (component/using [:handler]))))
+
+(defn -main [& _]
+  (let [config (config)]
+    (-> config
+        app-system
+        component/start)
+    (println "Started calculator on" (str "http://localhost:" (:http-port config)))))
---
+++ calculator/src/cljs/calculator/components/ui.cljs
@@ -0,0 +1,14 @@
+(ns calculator.components.ui
+  (:require [com.stuartsierra.component :as component]
+            [calculator.core :refer [render]]))
+
+(defrecord UIComponent []
+  component/Lifecycle
+  (start [component]
+    (render)
+    component)
+  (stop [component]
+    component))
+
+(defn new-ui-component []
+  (map->UIComponent {}))
